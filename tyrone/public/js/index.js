5f1ab7fa6ea9a7af3da5eabd03f57d8429aa337d
Tahir J <icodejs@gmail.com>
2012-12-30 19:57:30 +0000

    completed basic functionality of submitting a form and passing data to the server

--- tyrone/public/js/index.js
+++ tyrone/public/js/index.js
@@ -1,58 +1,63 @@
 
 (function ($, window, undefined) {
 
-    $(function () {
-        var
-        $submit = $('.submit'),
-        $reset  = $('.reset');
+  $(function () {
+    var
+    $form   = $('.nodeForm'),
+    $submit = $('.submit'),
+    $reset  = $('.reset');
 
-        $submit.on('click', function (e) {
-            e.preventDefault();
+    $submit.on('click', function (e) {
+      e.preventDefault();
 
-            var
-            $form          = $('.nodeForm'),
-            serialisedForm = $form.serializeArray(),
-            data           = getFormData(serialisedForm);
+      var
+      serialisedForm = $form.serializeArray(),
+      data           = getFormData(serialisedForm);
 
-            //console.log(emailIsValid(data.email));
-
-            $.ajax({
-              url      : '/save',
-              type     : 'POST',
-              dataType : 'json',
-              data     : data,
-              success  : function(data, textStatus, xhr) {
-                console.log(data.firstname);
-                console.log(data.lastname);
-                console.log(data.email);
-                console.log(data.password);
-              },
-              error    : function(xhr, textStatus, errorThrown) {
-                console.log(errorThrown);
-                console.log(textStatus);
-              }
-            });
+      if (formIsValid(data)) {
+        $.ajax({
+          url      : '/save',
+          type     : 'POST',
+          dataType : 'json',
+          data     : data,
+          success  : function(data, textStatus, xhr) {
+            console.log(data.firstname);
+            console.log(data.lastname);
+            console.log(data.email);
+            console.log(data.password);
+          },
+          error    : function(xhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+            console.log(textStatus);
+          }
         });
-
-        $reset.on('click', function (e) {
-            e.preventDefault();
-            $('input[type="text"], input[type="password"]').val('');
-        });
+      }
+      else {
+        console.log('invalid');
+      }
 
     });
 
-    function getFormData (formArray) {
-        var data = {};
+    $reset.on('click', function (e) {
+      e.preventDefault();
+      $form
+        .find('input[type="text"], input[type="password"]')
+        .val('');
+    });
 
-        $.map(formArray, function(item, i){
-            data[item['name']] = item['value'];
-        });
+  });
 
-        return data;
-    }
+  function getFormData (formArray) {
+      var data = {};
+      $.map(formArray, function(item, i){
+        data[item['name']] = item['value'];
+      });
+      return data;
+  }
 
-    // function emailIsValid (email){
-    //     return /^([a-zA-Z0-9_.-+])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/.test(email);
-    // }
+  function formIsValid(data) {
+    // form validation goes here
+    return true;
+  }
 
 }(jQuery, window));
